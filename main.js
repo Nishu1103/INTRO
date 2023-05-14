2804fc8c60b93e40372fe25bdbeeec63abc304c6
Nishu1103 <128515332+Nishu1103@users.noreply.github.com>
2023-05-14 21:42:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+<div class ="frame">
+        <a href="#" class="btn">
+        <i class="fab fa-facebook-f" style="color: #3b5998;"></i>
+        </a>
+        <a href="#" class="btn">
+        <i class="fab fa-twitter" style="color: #00acee;"></i>
+        </a>
+        <a href="#" class="btn">
+        <i class="fab fa-instagram" style="color: #eb14ae;"></i>
+        </a>
+        <a href="#" class="btn">
+        <i class="fab fa-linkedin-in" style="color:#0e76a8;"></i>
+        </a>
+        <a href="#" class="btn">
+        <i class="far fa-envelope"></i>
+        </a>
+</div>
